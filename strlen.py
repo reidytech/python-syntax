5dcc1c021be7737d39568a45f764fe461b55be3e
shsmathapp <shsmathapp@gmail.com>
2017-08-28 22:49:43 -0400

    WHAT???

--- strlen.py
+++ strlen.py
@@ -1,6 +1,6 @@
 #asks user to input string and tells them how long it is
 
-def askInput:
+def askInput():
     print("Please input a string")
-    userInput = raw_input() #raw_input() does not exist in 3.x, and has been replace by input()
-    print("Your string is ", str(len(userInput)), " long.")
+    userInput = input() #raw_input() does not exist in 3.x, and has been replace by input()
+    print("Your string is ", str(len(userInput)), " long.", sep='')
---
+++ tutorial.py
@@ -0,0 +1,87 @@
+def data_structures():
+        array1 = list()
+        array2 = []
+        hash_table1 = {}
+        hash_table2 = {}
+        string1 = ''
+        #literal
+        string2 = ""
+        #escape characters inside
+        num1 = 1
+        num2 = -120914312141234124
+        bool1 = True
+        bool2 = False
+        #static arrays, dynamically growing lists
+
+    class ArrayList(): #question oo
+        def __init__(self, max_length):
+            #constructor behaves as initializer
+#Arraylist doubles in size whenever you are adding to a static array more to when it's limit it
+
+            array = [int, int, int]
+
+        def append(self, input):
+            if(len(self.internal_array) )
+
+
+        array1.append(1)
+        array1.remove()
+        array1.insert('haha', 0)
+        assert array1[0] == 
+
+        hash1 = {}
+        hash1['color'] = 'blue'
+        print(hash1['color'])
+
+        assert '1' != 1
+
+        assert type('haha') == str 
+
+        if( some_function) == False:
+            throw SubElement(
+                
+                , tag, attrib={}, **extra)
+
+
+class Throwable:
+    def throw(self):
+        pass #statement 
+
+class CustomErrors:  #if customerros is a child of throwable
+    def throw(self): #pass the handler for the class itself as its first argument
+        if type(input) == int:
+            return 'isIntNotSTringError'
+        elif type(input) == bool:
+            return 'isBoolNotStringError'
+
+#pytest
+
+test_class = CustomErrors()
+test_class.throw(1)
+
+public class MainApplication(){
+    public static void main(Throwable CustomError){
+        System.out.println(CustomError);
+    }
+
+}
+
+#static means it is a class field, or method, as opposed to an instance level, you don't HAVE TO instantiate it
+
+private static class Helper() {
+    public static print(String input){
+        System.out.println("Customer String: " + Input); 
+    }
+}
+
+#static is class level, you don't need to create an instance of a class
+#field is more specific, all fields are variables, not all variables are fields, fields are variables that are part of a class
+
+
+#comments -> band-aid solutions, type, input
+
+
+        
+
+
+
