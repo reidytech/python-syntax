6affffb870af8e5809655386b0e88141adbebcdd
shsmathapp <shsmathapp@gmail.com>
2017-08-29 09:30:12 -0400

    Forgot what I did

---
+++ Stack.py
@@ -0,0 +1,13 @@
+class Stack():
+    def __init__(self):
+        self.size = 0
+        self.stack = []
+
+    def push(self, element):
+        defined = dir().count('element')
+            if(defined == 1)
+        self.size += 1
+        self.stack[0] = element
+            elif(defined != 1)
+        print('Data Element Not Defined')
+    
